2f07ee5ee6844c5ec686c4c86116ad233dd4c851
Dickens-odera <dickensodera9@gmail.com>
2020-02-20 18:17:45 +0300

    added url endpoints for the system users

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,29 @@ Route::delete('/deleteSchool/{school_id}','Staff\Schools\SchoolsController@destr
 //the student urls
 Route::prefix('student')->group(function()
 {
-    Route::get('/login',['as'=>'student.login.form','uses'=>'Auth\Student\StudentLoginController@showLoginForm']);
+    Route::get('/login-form',['as'=>'student.login.form','uses'=>'Auth\Student\StudentLoginController@showLoginForm']);
     Route::post('/login',['as'=>'student.login','uses'=>'Auth\Student\StudentLoginController@login']);
-    Route::get('/dashboard',['as'=>'student.dashboard','uses'=>'Student\StudentsController@index']);
+    Route::get('/dashboard',['as'=>'student.dashboard','uses'=>'Auth\Student\StudentLoginController@index']);
 });
+//the dean of school
+Route::prefix('dean')->group(function()
+{
+    Route::get('/login-form',['as'=>'dean.login.form','uses'=>'Auth\Deans\DeansLoginController@showLoginForm']);
+    Route::post('/login',['as'=>'dean.login','uses'=>'Auth\Deans\DeansLoginController@login']);
+    Route::get('/dashboard',['as'=>'dean.dashboard','uses'=>'Auth\Deans\DeansLoginController@index']);
+});
+//the registrar
+Route::prefix('registrar')->group(function()
+{
+    Route::get('/login-form',['as'=>'registrat.login.form','uses'=>'Auth\Registrar\RegistratLoginControler@showLoginForm']);
+    Route::pist('/login',['as'=>'registrar.login','uses'=>'Auth\Registrar\RegistrarLoginController@login']);
+    Route::get('/dashboard','Auth\Registrar\RegustrarLoginCotroller@index')->name('registrar.dashboard');
+});
+//the chairperson of department
+Route::prefix('cod')->group(function()
+{
+    Route::get('/login-form',['as'=>'cod.login.form','uses'=>'Auth\COD\CODLoginController@showLoginForm']);
+    Route::post('/login',['as'=>'cod.login','uses'=>'Auth\COD\CODLoginController@login']);
+    Route::get('/dashboard',['as'=>'cod.dashboard','uses'=>'Auth\COD\CODLoginController@index']);
+});
+
