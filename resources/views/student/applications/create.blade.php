f42efbf0b4303281214db714ce005195655495c7
Dickens-odera <dickensodera9@gmail.com>
2020-07-14 04:42:18 -0400

    installed smartWizard jQuery forms plugin

--- resources/views/student/applications/create.blade.php
+++ resources/views/student/applications/create.blade.php
@@ -12,6 +12,8 @@
 @extends('student.main')
 {{-- @include('student.header') --}}
 @section('content')
+<link href="https://cdn.jsdelivr.net/npm/smartwizard@5/dist/css/smart_wizard_all.min.css" rel="stylesheet" type="text/css" />
+
     <div class="box">
         <div class="box-header"></div>
             <div class="box-body">
@@ -507,7 +509,51 @@
             <div class="box-footer"></div>
         </div>
 
+        <script src="https://cdn.jsdelivr.net/npm/smartwizard@5/dist/js/jquery.smartWizard.min.js" type="text/javascript"></script>
+
         <script type="text/javascript">
-            var grade = Document.getElementById('grade');
+
+$('#smartwizard').smartWizard({
+    selected: 0, // Initial selected step, 0 = first step
+    theme: 'default', // theme for the wizard, related css need to include for other than default theme
+    justified: true, // Nav menu justification. true/false
+    darkMode:false, // Enable/disable Dark Mode if the theme supports. true/false
+    autoAdjustHeight: true, // Automatically adjust content height
+    cycleSteps: false, // Allows to cycle the navigation of steps
+    backButtonSupport: true, // Enable the back button support
+    enableURLhash: true, // Enable selection of the step based on url hash
+    transition: {
+        animation: 'none', // Effect on navigation, none/fade/slide-horizontal/slide-vertical/slide-swing
+        speed: '400', // Transion animation speed
+        easing:'' // Transition animation easing. Not supported without a jQuery easing plugin
+    },
+    toolbarSettings: {
+        toolbarPosition: 'bottom', // none, top, bottom, both
+        toolbarButtonPosition: 'right', // left, right, center
+        showNextButton: true, // show/hide a Next button
+        showPreviousButton: true, // show/hide a Previous button
+        toolbarExtraButtons: [] // Extra buttons to show on toolbar, array of jQuery input/buttons elements
+    },
+    anchorSettings: {
+        anchorClickable: true, // Enable/Disable anchor navigation
+        enableAllAnchors: false, // Activates all anchors clickable all times
+        markDoneStep: true, // Add done state on navigation
+        markAllPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
+        removeDoneStepOnNavigateBack: false, // While navigate back done step after active step will be cleared
+        enableAnchorOnDoneStep: true // Enable/Disable the done steps navigation
+    },
+    keyboardSettings: {
+        keyNavigation: true, // Enable/Disable keyboard navigation(left and right keys are used if enabled)
+        keyLeft: [37], // Left key code
+        keyRight: [39] // Right key code
+    },
+    lang: { // Language variables for button
+        next: 'Next',
+        previous: 'Previous'
+    },
+    disabledSteps: [], // Array Steps disabled
+    errorSteps: [], // Highlight step with errors
+    hiddenSteps: [] // Hidden steps
+});
         </script>
 @endsection
