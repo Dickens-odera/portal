8ddbb32cef904e8948d21e40939bc302b12f42b7
Dickens-odera <dickensodera9@gmail.com>
2020-07-15 07:49:09 -0400

    modified registrar incoming applications listings

--- resources/views/registrar/applications/incoming.blade.php
+++ resources/views/registrar/applications/incoming.blade.php
@@ -21,7 +21,7 @@
                             <th>Preffered Program</th>
                             <th>Preffered School</th>
                             <th>KCSE Index</th>
-                            <th>KCSE Year</th>
+                            {{-- <th>KCSE Year</th> --}}
                             {{-- <th>KUCCPS Password</th>
                             <th>Mean Grade</th>
                             <th>Aggregate Points</th>
@@ -48,20 +48,21 @@
                                 <td>{{ $value->preffered_program}}</td>
                                 <td>{{ $value->preffered_school}}</td>
                                 <td>{{ $value->kcse_index}}</td>
-                                <td>{{ $value->kcse_year}}</td>
+                                {{-- <td>{{ $value->kcse_year}}</td> --}}
                                 {{-- <td>{{ $value->kuccps_password}}</td>
                                 <td>{{ $value->mean_grade}}</td>
                                 <td>{{ $value->aggregate_points}}</td>
                                 <td>{{ $value->cut_off_points }}</td>
                                 <td>{{ $value->weighted_clusters}}</td> --}}
-                                <td style="color:lightblue">{{ $value->dean }}</td>
+                                <td style="color:red">{{ $value->dean }}</td>
                                 <td style="color:red">{{ $value->comment }}</td>
                                 {{-- <td>{{ $value->status}}</td> --}}
                                 <td>{{ \Carbon\Carbon::parse($value->created_at)->format('d/m/Y') }}</td>
                                 <td>{{ $value->app_type }}</td>
                                  <td class="btn-group btn-group-sm" style="width:100%">
-                                    <a href="{{ route('registrar.applications.incoming.single-view',['app_id'=>$value->app_id]) }}" class="btn btn-success btn-sm"><i class="fa fa-eye"></i></a>
-                                    <a href="" class="btn btn-sm btn-primary"><i class="fa fa-send"></i></a>
+                                    <a href="{{ route('registrar.applications.incoming.single-view',['app_id'=>$value->app_id]) }}" class="btn btn-success btn-sm"><i class="fa fa-eye"></i> View</a>
+                                    <a href="" class="btn btn-sm btn-primary"><i class="fa fa-send"></i> Approve</a>
+                                    <a href="" class="btn btn-sm btn-info"><i class="fa fa-wrench"></i></a>
                                     {{-- <form action="" method="post">
                                                {{ csrf_field() }}
                                                 <button class="btn btn-sm btn-primary" type="submit">
--- resources/views/student/sidebar.blade.php
+++ resources/views/student/sidebar.blade.php
@@ -16,41 +16,6 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
-        {{-- <li class="active treeview">
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-          </ul>
-        </li> --}}
-        {{-- <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
-            <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-          </ul>
-        </li> --}}
-        {{-- <li>
-          <a href="pages/widgets.html">
-            <i class="fa fa-th"></i> <span>Widgets</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-green">new</small>
-            </span>
-          </a>
-        </li> --}}
         <li class="treeview">
           <a href="#">
             <i class="fa fa-pie-chart"></i>
@@ -65,21 +30,6 @@
             <li><a href="{{ route('student.kuccps.programs') }}"><i class="fa fa-envelope"></i> Programs</a></li>
           </ul>
         </li>
-         {{-- <li class="treeview">
-          <a href="#">
-            <i class="fa fa-laptop"></i>
-            <span>Emergencies</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href=""><i class="fa fa-circle-o"></i> Accidents</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Maternity</a></li>
-            <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> First Aid</a></li>
-            {{-- <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-            <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-            <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li> --}}
           </ul>
         </li>
         {{-- <li class="treeview">
