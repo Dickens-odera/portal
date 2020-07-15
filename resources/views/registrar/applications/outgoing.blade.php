4aff381148cef9dcb366fcfd0878052157696db3
Dickens-odera <dickensodera9@gmail.com>
2020-07-15 08:17:55 -0400

    added registrar applications approval button

--- resources/views/registrar/applications/outgoing.blade.php
+++ resources/views/registrar/applications/outgoing.blade.php
@@ -61,7 +61,7 @@
                                 <td>{{ $value->app_type }}</td>
                                  <td class="btn-group btn-group-sm" style="width:100%">
                                     <a href="{{ route('registrar.applications.outgoing.single-view',['app_id'=>$value->app_id]) }}" class="btn btn-success btn-sm"><i class="fa fa-eye"></i></a>
-                                    <a href="" class="btn btn-sm btn-primary"><i class="fa fa-send"></i></a>
+                                    <a href="" class="btn btn-sm btn-primary"><i class="fa fa-send"></i> Approve</a>
                                     {{-- <form action="" method="post">
                                                {{ csrf_field() }}
                                                 <button class="btn btn-sm btn-primary" type="submit">
